e1f0547022a702a984aefd1ebc0045a64995a6a7
frezyl <hevesi.gergely@gmail.com>
2023-08-27 16:41:22 +0200

    add endpoint for testing sending a message

--- backend.py
+++ backend.py
@@ -1,4 +1,7 @@
-from fastapi import Depends, FastAPI
+import secrets
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import HTTPBasicCredentials, HTTPBasic
 
 app = FastAPI()
@@ -6,6 +9,33 @@ app = FastAPI()
 security = HTTPBasic()
 
 
+def authenticate(
+        credentials: Annotated[HTTPBasicCredentials, Depends(security)]
+):
+    is_username_correct = secrets.compare_digest(
+        credentials.username,
+        "test_user"
+    )
+    is_password_correct = secrets.compare_digest(
+        credentials.password,
+        "test_password"
+    )
+    if not (is_username_correct and is_password_correct):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect password or username"
+        )
+
+
 @app.get("/messages")
 async def get_messages():
     return {"messages": "Hello world"}
+
+
+@app.post("/messages")
+async def send_message(
+        message: str,
+        credentials: Annotated[HTTPBasicCredentials, Depends(security)]
+):
+    authenticate(credentials)
+    return {"messages": "Successful authentication"}
